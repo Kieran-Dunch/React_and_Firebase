9dfc04eda212f830b5c48ea9ec365bf0b52c9fc7
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-31 14:34:59 -0400

    onChange is great!

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -1,19 +1,27 @@
 import './NewEventForm.css'
+import {useState} from 'react'
 
 export default function NewEventForm() {
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
 
+  // const handleChange = (e) => {
+  //   setTitle(e.target.value)
+  // }
 
   return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-        <input type="text" />
+        <input type="text"onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Event Date: </span>
-        <input type="date" />
+        <input type="date" onChange={(e) => setDate(e.target.value)}/>
       </label>
       <button>Submit</button>
+      <p>Title - {title}</p>
+      <p>Date - {date}</p>
     </form>
   )
 }
