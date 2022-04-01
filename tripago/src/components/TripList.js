9776e191d8ef8c758c096b1c2062d3707653ef44
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-01 17:03:24 -0400

    Now we are making a custom hook!

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,18 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 // styles
 import './TripList.css'
 
 export default function TripList() {
-
   const [trips, setTrips] = useState([])
   const [url, setUrl] = useState('http://localhost:3000/trips')
 
+    const fetchTrips = useCallback(async () => {
+      const response = await fetch(url)
+      const json = await response.json()
+      setTrips(json)
+    }, [url])
+
+
   useEffect(() => {
-    fetch(url)
-      .then(response => response.json())
-      .then(json => setTrips(json))
-  }, [])
+    fetchTrips()
+  }, [url, fetchTrips])
 
 
   return (
@@ -26,6 +30,14 @@ export default function TripList() {
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+          European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+          All Trips
+        </button>
+      </div>
     </div>
   )
 }
