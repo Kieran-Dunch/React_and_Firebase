41515a8f438a97687016231565575c3b5b5528d2
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-04 17:08:48 -0400

    finished the router unit!

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,11 +1,21 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
+import { useEffect } from 'react'
 import { useFetch } from '../hooks/useFetch'
 
 export default function Article() {
   const { id } = useParams()
   const url = "http://localhost:3000/articles/" + id
-
   const {data: article , isPending, error } = useFetch(url)
+  const history = useHistory()
+
+  useEffect(() => {
+    if (error) {
+      // history.goBack()
+      setTimeout(() => {
+        history.push('/')
+      }, 2000)
+    }
+  }, [error, history])
 
   return (
     <div>
