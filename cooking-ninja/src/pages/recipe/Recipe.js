0c88092a8e92aef25da9093504692f2fc3890dc5
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-06 16:40:23 -0400

    finished the useFetch challenge!

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,7 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
 import './Recipe.css'
 
 export default function Recipe() {
+  const {id} = useParams()
+  const {data: recipe , isPending, error } = useFetch(`http://localhost:3000/recipes/${id}`)
+
   return (
-    <div>Recipe</div>
+    <div className="recipe">
+      {isPending && <p className="loading">Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {recipe && (
+        <h1>{recipe.title}</h1>
+      )}
+    </div>
   )
 }
