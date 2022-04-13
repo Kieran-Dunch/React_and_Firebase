f3b26a0bec5a48d4ead6e8fdff0fa242bf96f306
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-13 17:09:19 -0400

    finishing up the firebase module now, I am always impressed by the quality of teaching this course provides.

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -16,7 +16,7 @@ export default function Recipe() {
   useEffect(() => {
     setIsPending(true)
 
-    projectFirestore.collection('recipes').doc(id).get().then((doc) => {
+    const unsub = projectFirestore.collection('recipes').doc(id).onSnapshot((doc) => {
      if (doc.exists) {
        setIsPending(false)
        setRecipe(doc.data())
@@ -25,7 +25,16 @@ export default function Recipe() {
        setError('Could not find the recipe.')
      }
     })
+
+    return () => unsub()
+
   }, [id])
+
+  const handleClick = () => {
+    projectFirestore.collection('recipes').doc(id).update({
+      title: "updated recipe!"
+    })
+  }
 
   return (
     <div className={`recipe ${mode}`}>
@@ -39,6 +48,7 @@ export default function Recipe() {
             {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
           </ul>
           <p className="method">{recipe.method}</p>
+          <button onClick={handleClick}>Update me</button>
         </>
       )}
     </div>
