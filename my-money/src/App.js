ee991ce5d5004481727f66f6a98506c94fa87859
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-21 18:00:23 -0400

    finished creating login and signup forms.

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,5 +1,7 @@
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -10,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
