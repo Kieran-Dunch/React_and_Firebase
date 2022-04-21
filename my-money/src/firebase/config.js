888e5945bc6dee567c3094b52677089b98f1cb83
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-21 18:17:38 -0400

    init firebase auth and firestore

---
+++ my-money/src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCe5CCRd9LfNSYZRT4naj92vVHat8MksLc",
+  authDomain: "mymoney-54fb9.firebaseapp.com",
+  projectId: "mymoney-54fb9",
+  storageBucket: "mymoney-54fb9.appspot.com",
+  messagingSenderId: "807386937636",
+  appId: "1:807386937636:web:340baffc115d0eda6a1bb1"
+};
+
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
