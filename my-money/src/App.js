749bd97d37b67fccc148bf78bb3a35189118eb77
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-21 17:29:52 -0400

    created a router

---
+++ my-money/src/App.js
@@ -0,0 +1,29 @@
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
