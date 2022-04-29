89fb1416929d04e65e13125397dec9b040c2f8e6
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-28 20:32:17 -0400

    these new hooks we are building for login/logout are fascinationg

--- my-money/src/hooks/useSignup.js
+++ my-money/src/hooks/useSignup.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { projectAuth } from "../firebase/config";
-
+import {useAuthContext} from './useAuthContext'
 
 
 export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
 
   const signup = async (email, password, displayName) => {
     setError(null)
@@ -14,7 +15,6 @@ export const useSignup = () => {
     try {
       // signup user
       const response = await projectAuth.createUserWithEmailAndPassword(email, password)
-      console.log(response.user);
 
       if (!response) {
         throw new Error("Could not complete signup.")
@@ -22,6 +22,10 @@ export const useSignup = () => {
 
       // add display name to user
       await response.user.updateProfile({ displayName: displayName })
+
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: response.user })
+
 
       setIsPending(false)
       setError(null)
