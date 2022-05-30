78a0891b6f15366cfddd0a05879c229989f070fb
Kieran Dunch <dunch.kieran@gmail.com>
2022-05-30 15:01:36 -0400

    finished firebase queries

--- my-money/src/hooks/useCollection.js
+++ my-money/src/hooks/useCollection.js
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { projectFirestore } from "../firebase/config"
 
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null)
   const [error, setError] = useState(null)
 
+  // if useRef not in use, then this would create an infinite loop
+  // _query is an array that is different on each method call
+  const query = useRef(_query).current
+
   useEffect(() => {
     let ref = projectFirestore.collection(collection)
+
+    if (query) {
+      ref = ref.where(...query)
+    }
 
     const unsubscribe = ref.onSnapshot((snapshot) => {
       let results = []
@@ -26,7 +34,7 @@ export const useCollection = (collection) => {
     // unsubscribe on unmount
     return () => unsubscribe()
 
-  }, [collection])
+  }, [collection, query])
 
 
   return { documents, error }
