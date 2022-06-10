04ebb2ebd0db57867111283722148560830a4cee
Kieran Dunch <dunch.kieran@gmail.com>
2022-06-10 11:52:30 -0400

    started card choice logic

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -4,17 +4,19 @@ import SingleCard from './components/SingleCard'
 import './App.css'
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"},
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" },
 ]
 
 function App() {
   const[cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   // shuffle cards
   // randomize the cards
@@ -34,7 +36,10 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
     <div className='card-grid'>
       {cards.map(card => (
-        <SingleCard key={card.id} card={card}/>
+        <SingleCard
+          key={card.id}
+          card={card}
+        />
       ))}
     </div>
     </div>
