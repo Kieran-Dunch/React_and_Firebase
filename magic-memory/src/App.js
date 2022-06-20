dad4a131bd0d3eda1f658932bbc6c4912dbaa972
Kieran Dunch <dunch.kieran@gmail.com>
2022-06-19 20:50:40 -0400

    matching card exercise!

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -4,12 +4,12 @@ import SingleCard from './components/SingleCard'
 import './App.css'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 
 function App() {
@@ -38,11 +38,20 @@ function App() {
   // make a comparison function
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+
       if(choiceOne.src === choiceTwo.src) {
-        console.log("those cards match!")
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return  { ...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn()
       } else {
-        console.log("those cards don't match")
+
         resetTurn()
       }
     }
